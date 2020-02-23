d2599330b763662d109e3dc5634c81caa0eb0012
Ravi Kumar <ravithesun02@gmail.com>
2020-02-23 09:59:19 +0530

    old one

--- components/camera/cameracomponent.js
+++ components/camera/cameracomponent.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, { PureComponent } from 'react';
-import {StyleSheet, Text,  View } from 'react-native';
+import {StyleSheet, Text,  View,TouchableOpacity } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Button,Icon } from 'native-base';
@@ -19,13 +19,23 @@ const PendingView = () => (
 );
 
 class Camera extends PureComponent {
+
+  constructor(props){
+    super(props);
+    this.state={
+      cameraType:RNCamera.Constants.Type.back,
+      Flash:RNCamera.Constants.FlashMode.auto,
+      recordingVideo:false
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <RNCamera
           style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
+          type={this.state.cameraType}
+          flashMode={this.state.Flash}
           androidCameraPermissionOptions={{
             title: 'Permission to use camera',
             message: 'We need your permission to use your camera',
@@ -38,21 +48,22 @@ class Camera extends PureComponent {
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
           }}
+          defaultVideoQuality={RNCamera.Constants.VideoQuality["720p"]}
         >
-          {({ camera, status }) => {
+          {({ camera, status,recordAudioPermissionStatus }) => {
             if (status !== 'READY') return <PendingView />;
             return (
-              <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, marginBottom: 15, alignItems: 'flex-end' }}>
-             
-
-              <View style={{ alignItems: 'center' }}>
-                <Button transparent onPress={()=>this.takePicture.bind(this)}>
-                  <Text>Click</Text>
-                  </Button>
-                 
-              </View>
-
-          </View>
+              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+              <TouchableOpacity onPress={() => this.state.recordingVideo ? this.stopRecording(camera) : this.takePicture(camera)} onLongPress={()=>this.recordVideo(camera)} style={styles.capture}>
+                <Text style={{ fontSize: 14 }}> SNAP </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=>this.toggleFlash()} >
+                <Text >Flash Togler</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={()=>this.toggleCamera()} >
+                <Text >camera toggler</Text>
+              </TouchableOpacity>
+            </View>
             );
           }}
         </RNCamera>
@@ -60,14 +71,83 @@ class Camera extends PureComponent {
     );
   }
 
+  toggleCamera(){
+    if(this.state.cameraType==RNCamera.Constants.Type.back)
+    {
+      this.setState({
+        cameraType:RNCamera.Constants.Type.front
+      });
+    }
+    else
+    this.setState({
+      cameraType:RNCamera.Constants.Type.back
+    });
+
+    console.log(this.state.cameraType);
+  }
+
   takePicture = async function(camera) {
     const options = { quality: 0.5, base64: true };
     const data = await camera.takePictureAsync(options);
     //  eslint-disable-next-line
-    console.log("gaandu");
     console.log(data.uri);
   };
+
+  toggleFlash(){
+    console.log(this.state.Flash);
+    if(this.state.Flash==RNCamera.Constants.FlashMode.auto)
+    {
+      this.setState({
+        Flash:RNCamera.Constants.FlashMode.on
+      });
+      console.log(this.state.Flash);
+    }
+   else if(this.state.Flash==RNCamera.Constants.FlashMode.on)
+    {
+      this.setState({
+        Flash:RNCamera.Constants.FlashMode.off
+      });
+      console.log(this.state.Flash);
+    }
+    else
+    {
+      this.setState({
+        Flash:RNCamera.Constants.FlashMode.auto
+      });
+      console.log(this.state.Flash);
+    }
+    
+  }
+
+  recordVideo=async function(camera){
+    const options={
+      fixOrientation: true,
+      forceUpOrientation: true,
+      videoOrientation: 1,
+      deviceOrientation: 1,
+    };
+    this.setState({
+      recordVideo:true
+    });
+    console.log("Recording starts");
+    const { uri, codec } = await camera.recordAsync(options);
+
+    console.log(uri);
+
+  }
+
+  stopRecording= function(camera){
+   const data= camera.stopRecording();
+    this.setState({
+      recordVideo:false
+    });
+
+    console.log("finished recording");
+    console.log(data.uri);
+  }
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
